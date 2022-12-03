e46a989050f2e193e19f034e94083c74536ba816
cxy <itmanyong@gmail.com>
2022-12-03 17:06:52 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,8 +10,8 @@ const config = {
     lang: 'zh-CN',
     description: '整理汇集前端开发者资源,包含 WEB H5 小程序 学习资料 等',
     head: createHead(),
+    lastUpdated: true,
     themeConfig: {
-        lastUpdated: true,
         lastUpdatedText: '最近更新时间',
         outlineTitle: '本页面',
         // algolia检索-https://docsearch.algolia.com/
