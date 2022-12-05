5fdac4a9462cbac9203539f829c9de373df2b93a
itmanyong <itmanyong@gmail.com>
2022-12-05 03:04:25 +0000

    Deploying to gh-pages from @ itmanyong/web-resources@7353ac8f7b0f783325825492041930dffe601f0f 🚀

---
+++ assets/platform_fc_ecology_style.md.db2a0bc4.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.70f0c86d.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"platform/fc/ecology/style.md","lastUpdated":1670209423000}'),o={name:"platform/fc/ecology/style.md"};function r(s,c,n,l,p,d){return a(),t("div")}const m=e(o,[["render",r]]);export{_ as __pageData,m as default};
