0b8aefede0864a30b4e4d1a07920917bd834d31a
itmanyong <itmanyong@gmail.com>
2022-12-06 10:03:30 +0000

    Deploying to gh-pages from @ itmanyong/web-resources@b8de10f84d0c817ddaebef66d95274e5b3675cfb 🚀

---
+++ assets/platform_fc_tool_function.md.7866a3ac.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./app.766e6bcf.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"platform/fc/tool/function.md","lastUpdated":1670320973000}'),a={name:"platform/fc/tool/function.md"};function n(c,r,s,f,p,i){return o(),e("div")}const l=t(a,[["render",n]]);export{d as __pageData,l as default};
---
+++ assets/platform_tp_plugin.md.c735ed5d.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./app.766e6bcf.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"platform/tp/plugin.md","lastUpdated":1670320973000}'),r={name:"platform/tp/plugin.md"};function o(p,n,s,c,i,l){return a(),e("div")}const m=t(r,[["render",o]]);export{_ as __pageData,m as default};
